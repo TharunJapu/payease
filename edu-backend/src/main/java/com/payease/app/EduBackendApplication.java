0dff31a6cfd638d700c7cf2406108174d9056106
tharun.japu <tharun.japu@toucanus.com>
2025-08-02 16:27:25 +0530

    Added properties

--- edu-backend/src/main/java/com/payease/app/EduBackendApplication.java
+++ edu-backend/src/main/java/com/payease/app/EduBackendApplication.java
@@ -2,12 +2,23 @@ package com.payease.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EduBackendApplication {
+public class EduBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EduBackendApplication.class, args);
+		SpringApplication app = new SpringApplication(EduBackendApplication.class);
+		String activeProfile = System.getProperty("spring.profiles.active");
+
+		if (activeProfile == null || activeProfile.isEmpty()) {
+			activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
+		}
+
+		if (activeProfile == null || activeProfile.isEmpty()) {
+			app.setAdditionalProfiles("dev");
+		}
+		app.run(args);
 	}
 
 }
